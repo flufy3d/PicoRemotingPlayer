1c93fd2859efb501109a13f517bef7a8da5c5ffb
flufy3d <flufy3d@gmail.com>
2023-10-06 00:12:04 +0200

    add cmd buffer

---
+++ pico_remoting_player/render/cmd_buffer.h
@@ -0,0 +1,58 @@
+// cmd_buffer.h
+#pragma once
+
+#include "vulkan_helpers.h"
+
+
+class CmdBuffer {
+
+#define LIST_CMDBUFFER_STATES(_) \
+    _(Undefined)                 \
+    _(Initialized)               \
+    _(Recording)                 \
+    _(Executable)                \
+    _(Executing)
+    enum class CmdBufferState {
+#define MK_ENUM(name) name,
+        LIST_CMDBUFFER_STATES(MK_ENUM)
+#undef MK_ENUM
+    };
+    
+public:
+    CmdBuffer();
+    CmdBuffer(const CmdBuffer&) = delete;
+    CmdBuffer& operator=(const CmdBuffer&) = delete;
+    CmdBuffer(CmdBuffer&&) = delete;
+    CmdBuffer& operator=(CmdBuffer&&) = delete;
+    ~CmdBuffer();
+
+    bool Init(VkDevice device, uint32_t queueFamilyIndex);
+    bool Begin();
+    bool End();
+    bool Exec(VkQueue queue);
+    bool Wait();
+    bool Reset();
+
+public:
+    VkCommandBuffer buf{VK_NULL_HANDLE};
+
+private:
+    void SetState(CmdBufferState newState);
+
+    CmdBufferState state{CmdBufferState::Undefined};
+    VkCommandPool pool{VK_NULL_HANDLE};
+    
+    VkFence execFence{VK_NULL_HANDLE};
+    VkDevice m_vkDevice{VK_NULL_HANDLE};
+
+    std::string StateString(CmdBufferState s);
+#define CHECK_CBSTATE(s)                                                                                           \
+    do                                                                                                             \
+        if (state != (s)) {                                                                                        \
+            Log::Write(Log::Level::Error,                                                                          \
+                       std::string("Expecting state " #s " from ") + __FUNCTION__ + ", in " + StateString(state)); \
+            return false;                                                                                          \
+        }                                                                                                          \
+    while (0)
+
+};
