e171cb725e1bb6a0e01e07a0a6daa9412f6d7899
flufy3d <flufy3d@gmail.com>
2023-10-09 00:12:11 +0200

    add depth buffer file

---
+++ pico_remoting_player/render/depth_buffer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "vulkan_helpers.h"
+#include "memory_allocator.h"
+#include "cmd_buffer.h"
+
+
+struct DepthBuffer {
+    VkDeviceMemory depthMemory{VK_NULL_HANDLE};
+    VkImage depthImage{VK_NULL_HANDLE};
+
+    DepthBuffer() = default;
+    ~DepthBuffer();
+    DepthBuffer(DepthBuffer&& other) noexcept;
+    DepthBuffer& operator=(DepthBuffer&& other) noexcept;
+    void Create(VkDevice device, MemoryAllocator* memAllocator, VkFormat depthFormat,
+                const XrSwapchainCreateInfo& swapchainCreateInfo);
+    void TransitionLayout(CmdBuffer* cmdBuffer, VkImageLayout newLayout);
+
+    DepthBuffer(const DepthBuffer&) = delete;
+    DepthBuffer& operator=(const DepthBuffer&) = delete;
+
+private:
+    VkDevice m_vkDevice{VK_NULL_HANDLE};
+    VkImageLayout m_vkLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+};
